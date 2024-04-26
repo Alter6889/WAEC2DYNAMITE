1fc61a7ee61188bd5edfe5aeb43b66403bf4f56b
Alter6889 <etereolaider@gmail.com>
2024-04-26 16:51:18 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/WAEC2Dynamite/Waec2DynamiteApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Dynamite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2DynamiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2DynamiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/WAEC2Dynamite/Waec2DynamiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Dynamite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2DynamiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
